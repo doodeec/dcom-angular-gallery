a0f802e95a580d468a9d513910d780c3a187d00a
doodeec <doodeec@gmail.com>
2014-01-03 20:08:43 +0100

    version 0.0.2
    small changes in index.html, lazy loading of thumbnails

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ##Version
 
-0.0.1 beta
+0.0.2
 
 #Description
 
--- src/dcomGallery.js
+++ src/dcomGallery.js
@@ -2,8 +2,16 @@
 
 angular.module('dcomGallery', [])
     .directive('dcomGalleryPic',
-        [
-            function() {
+        ['$window',
+            function($window) {
+                var isVisible = function(elem) {
+                    var pos = elem.position().top,
+                        winH = window.innerHeight,
+                        scrollY = window.scrollY;
+
+                    return (pos > scrollY && pos < winH + scrollY);
+                };
+
                 return {
                     restrict: 'A',
                     replace: true,
@@ -16,15 +24,28 @@ angular.module('dcomGallery', [])
                         scope.resolvedPic = null;
                         scope.orientation = null;
 
-                        var image = new Image(),
-                            link = "images/thumb/" +scope.pic+ ".jpg";
-                        image.onload = function() {
-                            scope.$apply(function(){
-                                scope.orientation = image.width > image.height ? "landscapeImg" : "portraitImg";
-                                scope.resolvedPic = link;
-                            });
+                        var loadingStarted = false;
+
+                        var checkVisibility = function() {
+                            if (isVisible(elem) && !loadingStarted) loadImage();
                         };
-                        image.src = link;
+
+                        var loadImage = function() {
+                            loadingStarted = true;
+                            var image = new Image(),
+                                link = "images/thumb/" +scope.pic+ ".jpg";
+                            image.onload = function() {
+                                scope.$apply(function(){
+                                    scope.orientation = image.width > image.height ? "landscapeImg" : "portraitImg";
+                                    scope.resolvedPic = link;
+                                });
+                            };
+                            image.src = link;
+                        };
+
+                        checkVisibility();
+
+                        angular.element($window).bind("scroll", checkVisibility);
 
                         elem.bind('click', function(e) {
                             e && e.preventDefault();
